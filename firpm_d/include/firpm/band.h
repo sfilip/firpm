5253495b021fbe0025189831cee586e7618a86e3
sfilip <silviu.filip@inria.fr>
2019-11-11 20:55:36 +0100

    refactoring of band.h and band.cpp

--- firpm_d/include/firpm/band.h
+++ firpm_d/include/firpm/band.h
@@ -6,7 +6,7 @@
  * */
 
 //    firpm_d
-//    Copyright (C) 2015  S. Filip
+//    Copyright (C) 2015 - 2019  S. Filip
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -28,34 +28,38 @@
 #include "util.h"
 
 /**
- * The interval type being considered (in \f$\left[0, \pi\right]\f$ or in \f$\left[-1, 1\right]\f$)
+ * The interval type being considered (in \f$\left[0, \pi\right]\f$ 
+ * or in \f$\left[-1, 1\right]\f$)
  */
-enum BandSpace {
-    FREQ,           /**< not done the change of variable (i.e. we are in \f$\left[0, \pi\right]\f$) */
-    CHEBY           /**< done the change of variable (i.e. we are in \f$\left[-1, 1\right]\f$) */
+enum space_t {
+    FREQ,           /**< not done the change of variable 
+                    (i.e., we are in \f$\left[0, \pi\right]\f$) */
+    CHEBY           /**< done the change of variable 
+                    (i.e., we are in \f$\left[-1, 1\right]\f$) */
 };
 
 /**
- * @brief A data type encapsulating information relevant to a frequency band
+ * @brief A data type encapsulating information relevant to a 
+ * frequency band
  *
- * Contains important information concerning a frequency band used during the
- * execution of the exchange algorithm.
+ * Contains important information concerning a frequency band 
+ * used during the execution of the exchange algorithm.
  */
-struct Band {
-    BandSpace space;        /**< the space in which we are working */
+struct band_t {
+    space_t space;          /**< the space in which we are working */
     std::function<double(BandSpace, double)> amplitude;
                             /**< the ideal amplitude for this band */
     double start;           /**< the left bound of the band */
     double stop;            /**< the right bound of the band */
     std::function<double(BandSpace, double)> weight;
                             /**< weight function value on the band */
-    std::size_t extremas;   /**< number of interpolation points taken in the band */
+    std::size_t xs;         /**< number of interpolation points taken in the band */
 };
 
 /**
  * Gives the direction in which the change of variable is performed
  */
-enum ConversionDirection {
+enum convdir_t {
     FROMFREQ,               /**< apply the change of variable \f$y=\cos(x)\f$*/
     TOFREQ                  /**< apply the change of variable \f$y=\arccos(x)\f$*/
 };
@@ -63,9 +67,10 @@ enum ConversionDirection {
 /*! Performs the change of variable on the set of bands of interest
  * @param[out] out output frequency bands
  * @param[in]  in input frequency bands
- * @param[in]  direction the direction in which the change of variable is performed
+ * @param[in]  direction the direction in which the change of 
+ * variable is performed
  */
-void bandConversion(std::vector<Band> &out, std::vector<Band> &in,
-        ConversionDirection direction);
+void bandconv(std::vector<Band> &out, std::vector<Band> &in,
+        convdir_t direction);
 
 #endif /* BAND_H_ */
